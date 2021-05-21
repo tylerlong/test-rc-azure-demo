4e2bae97bbd4e454bf5ddf6dd6f116ebfb2f587a
Tyler Long <tyler4long@gmail.com>
2021-05-21 12:14:54 -0700

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello world');
+import axios from 'axios';
+import waitFor from 'wait-for-async';
+
+const url =
+  'https://syncquickbasetoringcentral.azurewebsites.net/api/CallRingCentral';
+
+(async () => {
+  let count = 0;
+  let successCount = 0;
+  let failureCont = 0;
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    count += 1;
+    const r = await axios.get(url);
+    const data = r.data;
+    if (data.indexOf('StatusCode: 401') !== -1) {
+      successCount += 1;
+    } else {
+      failureCont += 1;
+    }
+    console.log(
+      `Tried ${count} times, success: ${successCount}, failure: ${failureCont}`
+    );
+    await waitFor({interval: 5000});
+  }
+})();
